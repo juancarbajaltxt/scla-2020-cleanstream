e8dcc454dabdd6baa3abb1e6d66e5774be2b78ef
Juan Carbajal <txt-52@MacBook-Pro-5.lan1>
2020-07-14 11:16:51 -0700

    testflight version

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import {NavController} from 'ionic-angular';
 import {AreaPage} from '../area/area.page';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,7 +10,7 @@ import {AreaPage} from '../area/area.page';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  constructor(private nav : NavController) { }
 
   //pushPage() {
     //this.navCtrl.push(AreaPage)
